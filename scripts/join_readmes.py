7ab8e619176714b72692d45d6ae8abac7759357f
KuligKamil <kamil.marek.kulig@gmail.com>
2024-08-17 01:03:07 +0200

    #14 chore: add order for joing script, clear old readme & add todo about assets

--- scripts/join_readmes.py
+++ scripts/join_readmes.py
@@ -1,12 +1,30 @@
 import os
 
+# TODO: move assets files from tutorial directory to main directory folder assets
+order = (
+    "basic_readme",
+    "introduction",
+    "why_mongodb",
+    "python_mongodb_tools",
+    "project_setup",
+    "mongodb_atlas",
+    "connection_to_db",
+    "data_structure",
+    "crud_queries",
+    "data_generators",
+    "tests",
+    "resources",
+)
+
 project_directory = os.getcwd()
-for root, _, f in os.walk(f"{project_directory}/tutorial"):
-    for file in f:
-        if file.endswith(".md"):
-            temp_text = ""
-            with open(os.path.join(root, file)) as f:
-                temp_text = f.read()
-            with open(f"{project_directory}/README.md", "a") as f:
-                f.write(f"\n\n{temp_text}")
-# TODO: add order of flies
+new_line = "\n"
+if os.path.exists(f"{project_directory}/README.md"):
+    os.remove(f"{project_directory}/README.md")
+for index, tutorial_chapter in enumerate(order):
+    temp_text = ""
+    with open(
+        os.path.join(f"{project_directory}/tutorial/{tutorial_chapter}", "README.md")
+    ) as f:
+        temp_text = f.read()
+    with open(f"{project_directory}/README.md", "a") as f:
+        f.write(f"{ new_line * 2 if index > 0 else ''}{temp_text}")
